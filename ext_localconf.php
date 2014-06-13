d32120fb021609a7321130c0f9f11f16c36efae5
Xavier Perseguers <xavier@causal.ch>
2014-06-13 15:00:46 +0200

    [TASK] Better integration in TYPO3

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,3 +5,6 @@ if (!defined ('TYPO3_MODE')) {
 
 // Add plugin
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_tscobj_pi1.php','_pi1','list_type',1);
+
+// Hook in Web > Page
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info'][$_EXTKEY . '_pi1'][] = 'EXT:' . $_EXTKEY . '/pi1/class.tx_tscobj_pi1_info.php:tx_tscobj_pi1_info->getInfo';
