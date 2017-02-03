63728000c7909039bfbab8db99ac5b47e186b03a
Xavier Perseguers <xavier@causal.ch>
2017-02-03 17:19:35 +0100

    [TASK] Mark extension as compatible with PHP 7.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.3.0-dev',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.3.0-5.6.99',
+            'php' => '5.3.3-7.1.99',
             'typo3' => '6.2.0-7.99.99',
         ),
         'conflicts' => array(),
