babbbf1c871472244de50d4940c1e1e60445c5a2
Matthias Vogel <git@kanti.de>
2019-09-27 09:17:53 +0200

    [TASK] Mark extension as compatible with PHP 7.3 (#16)

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.3.1',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.3.3-7.2.99',
+            'php' => '5.3.3-7.3.99',
             'typo3' => '6.2.0-8.99.99',
         ),
         'conflicts' => array(),
