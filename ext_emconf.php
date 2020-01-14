92e467b668974d2a9120728458f7c28fe606ce8f
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2020-01-14 16:41:14 +0100

    Release v1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "tscobj".
- *
- * Auto generated 29-10-2014 18:51
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Content element from TypoScript',
     'description' => 'A plugin which lets you use any TypoScript object as a normal content element.',
@@ -24,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.0.0-7.2.99',
+            'php' => '7.0.0-7.99.99',
             'typo3' => '8.7.0-8.99.99',
         ],
         'conflicts' => [],
