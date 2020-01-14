17d8c6717532ad061c40907ee63b0cbf9b7f3680
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2020-01-14 16:26:03 +0100

    Upgrade extension for TYPO3 8 (#15)

--- Modules/Wizards/TypoScriptBrowserWizard/conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-//required for mod.php
-$MCONF['name'] = 'wizard_typoscript_browser';
-$MCONF['script'] = '_DISPATCH';
-$MCONF['access'] = '';
--- ext_tables.php
+++ ext_tables.php
@@ -1,32 +1,14 @@
 <?php
-defined('TYPO3_MODE') || die ('Access denied.');
-
-// Plugin options
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages,recursive';
-
-// Add flexform fields to plugin options
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
+defined('TYPO3_MODE') || die();
 
 // Add flexform DataStructures
-if (version_compare(TYPO3_branch, '8.0', '>=')) {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_ds_pi1.xml');
-} else {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_ds_pi1.v7.xml');
-}
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'tscobj_pi1',
+    'FILE:EXT:tscobj/Configuration/FlexForms/flexform_ds_pi1.xml'
+);
 
 // Add plugins
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin([
     'LLL:EXT:tscobj/Resources/Private/Language/locallang_db.xlf:tt_content.list_type_pi1',
-    $_EXTKEY . '_pi1'
-), 'list_type');
-
-if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['tx_' . $_EXTKEY . '_pi1_wizicon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Controller/Pi1/class.tx_tscobj_pi1_wizicon.php';
-
-    if (version_compare(TYPO3_branch, '8.0', '<')) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath(
-            'wizard_typoscript_browser',
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Modules/Wizards/TyposcriptBrowserWizard/'
-        );
-    }
-}
+    'tscobj_pi1'
+], 'list_type');
