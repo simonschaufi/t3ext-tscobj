64889ad9e4d58e65d5d94acf43810c9a79dc9f47
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2026-06-15 16:58:39 +0200

    [TASK] Apply SafeDeclareStrictTypesRector

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Content element from TypoScript',
     'description' => 'A plugin which lets you use any TypoScript object as a normal content element.',
